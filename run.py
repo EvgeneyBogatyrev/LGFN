492bca76f66ed754c078f942b16764fb7da1fa11
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-20 01:08:31 +0300

    Fix multiple input

--- run.py
+++ run.py
@@ -1,15 +1,7 @@
 import os
 
 os.system("chmod -R 0777 /model")
-'''
-with open('/model/config.sh', 'w') as g:
-    g.write('cd /model\n')
-    g.write("chmod 0777 LGFN.sh\n")
-    g.write('./LGFN.sh\n')
 
-os.system('chmod 0777 /model/config.sh')
-os.system('/model/config.sh')
-'''
 with open('/model/run.sh', 'w') as f:
     f.write("mkdir /model/result\n")
 
@@ -19,7 +11,8 @@ with open('/model/run.sh', 'w') as f:
     vids = os.listdir("/dataset")
 
     for vid in vids:
-        f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /model/result\n")
+        Path(f"/model/results/{vid}").mkdir(parents=True, exist_ok=True)
+        f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /model/result/{vid}\n")
         
     f.write("chmod -R 0777 /model\n")
     
