287a1607fedd1006823e0ea4c798b2a19021dbb8
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:24:09 +0300

    Change interface

--- run.py
+++ run.py
@@ -4,18 +4,16 @@ from pathlib import Path
 os.system("chmod -R 0777 /model")
 
 with open('/model/run.sh', 'w') as f:
-    f.write("mkdir /model/result\n")
-
     f.write('cd /model/src/Deform_Conv\n')
     f.write('python setup.py build install\n')
     
     vids = os.listdir("/dataset")
 
     for vid in vids:
-        Path(f"/model/results/{vid}").mkdir(parents=True, exist_ok=True)
-        f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /model/result/{vid}\n")
+        f.write(f"mkdir /results/{vid}\n")
+        f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /results/{vid}\n")
         
-    f.write("chmod -R 0777 /model\n")
+    f.write("chmod -R 0777 /results\n")
     
 os.system('chmod 0777 /model/run.sh')
 os.system('sh /model/run.sh')
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-mkdir /model/result
 cd /model/src/Deform_Conv
 python setup.py build install
-python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name gauss --save_dir /model/result/gauss
-python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name bicubic --save_dir /model/result/bicubic
-chmod -R 0777 /model
+mkdir /results/bicubic
+python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name bicubic --save_dir /results/bicubic
+chmod -R 0777 /results
