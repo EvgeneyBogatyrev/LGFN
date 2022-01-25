9156200c39b22cc93ec03b01b8f5ee972e73f46b
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 22:04:22 +0300

    Now runs without root

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-os.system("chmod -R 0777 /model")
 
 with open('/model/run.sh', 'w') as f:
     f.write('cd /model/src/Deform_Conv\n')
@@ -13,7 +12,5 @@ with open('/model/run.sh', 'w') as f:
         f.write(f"mkdir /results/{vid}\n")
         f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /results/{vid}\n")
         
-    f.write("chmod -R 0777 /results\n")
     
-os.system('chmod 0777 /model/run.sh')
 os.system('sh /model/run.sh')
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 cd /model/src/Deform_Conv
 python setup.py build install
+mkdir /results/gauss
+python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name gauss --save_dir /results/gauss
 mkdir /results/bicubic
 python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name bicubic --save_dir /results/bicubic
-chmod -R 0777 /results
