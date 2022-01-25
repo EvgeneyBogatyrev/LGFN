04bc4c93f881ec2ee3c258a07fc433d1db728cb8
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 22:33:45 +0300

    Fix

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ with open('/model/run.sh', 'w') as f:
     vids = os.listdir("/dataset")
 
     for vid in vids:
-        f.write(f"mkdir /results/{vid}\n")
+        f.write(f"mkdir /result/{vid}\n")
         f.write(f"python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name {vid} --save_dir /results/{vid}\n")
         
     
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd /model/src/Deform_Conv
 python setup.py build install
-mkdir /results/gauss
+mkdir /result/gauss
 python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name gauss --save_dir /results/gauss
-mkdir /results/bicubic
+mkdir /result/bicubic
 python3 /model/src/main.py --test_only --save_results --pre_train /model/model/LGFN_x4.pt --dir_demo /dataset --video_name bicubic --save_dir /results/bicubic
